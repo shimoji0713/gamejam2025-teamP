3f76f575bb72b73cb95855b59f1c0942a5e28180
Norisuke1028 <138739405+Norisuke1028@users.noreply.github.com>
2025-02-26 10:58:36 +0900

    カウントにSE追加
    カウントが5増えるたびにSEが鳴るようにした

--- GameJam2025-teamP/Counter.h
+++ GameJam2025-teamP/Counter.h
@@ -6,14 +6,17 @@ class Counter
 {
 private:
 	int b_count;			// ボタンが押された回数
+	int b_lastcount;        //最後に鳴らしたカウント
 
 	int value_decrease_count;		//連打値の減少
 
 	int counter_time;		//時間制限
 
+
 private:
 	int anim[4];  //プレイヤーのアニメーション
 	int image;  //画像
+	int chage_se;  //チャージ音
 	int animation_count;  //カウント
 
 public:
@@ -38,6 +41,5 @@ public:
 
 	//ボタンを押した回数によってアニメーションを変える
 	void Counter_Animation();
-
 };
 
